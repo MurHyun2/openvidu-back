363c9cb0ee84e11b118fc7120f26514b1c43ba37
이대현 <eddy1219@naver.com>
2025-02-04 14:33:22 +0900

    3

--- src/main/java/io/openvidu/basic/java/WebConfig.java
+++ src/main/java/io/openvidu/basic/java/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React의 로컬 주소
+                .allowedOrigins("*") // React의 로컬 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
